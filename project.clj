f262c2281b449015e2772893fd112832789dab53
sunkencity <tux@well.com>
2012-11-12 10:04:17 +0100

    .

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dr-textile-and-mr-hiccup "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Converter for subset of textile into hiccup for light formatting"
+  :url "https://github.com/sunkencity/dr-textile-and-mr-hiccup/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
